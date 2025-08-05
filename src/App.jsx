ecf41f118c0f1c8f23424ea0014c1b1695884f51
НовыйИмя <Email>
2025-08-05 04:05:01 +0200

    fix: updated deps and set Node.js version

--- src/App.jsx
+++ src/App.jsx
@@ -4,9 +4,7 @@ import Home from "./pages/Home";
 const App = () => {
   return (
     <Routes>
-      <Routes>
         <Route path="/" element={<Home />} />
-      </Routes>
     </Routes>
   );
 };
