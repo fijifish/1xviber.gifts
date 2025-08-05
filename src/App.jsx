2eb6acf234242d9ec870bd356fbcdc3eb093ac19
НовыйИмя <Email>
2025-08-05 04:04:26 +0200

    fix: updated deps and set Node.js version

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,13 @@
-// src/App.jsx
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 
 const App = () => {
   return (
-    <Router>
+    <Routes>
       <Routes>
         <Route path="/" element={<Home />} />
       </Routes>
-    </Router>
+    </Routes>
   );
 };
 
