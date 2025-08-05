7c520ed8050ea063caa45de03f752e9289f6e480
НовыйИмя <Email>
2025-08-05 03:47:20 +0200

    fix: updated deps and set Node.js version

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
+  base: './', // <== 🔥 вот это важно!
   server: {
     headers: {
       "Access-Control-Allow-Origin": "*",
