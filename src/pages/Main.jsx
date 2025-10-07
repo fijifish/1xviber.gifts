1afc919016b250d24a51bcc336c0ec9cf3922252
НовыйИмя <Email>
2025-10-07 17:23:46 +0200

    ddd

--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -201,7 +201,7 @@ const OnexGifts = () => {
                     <div className="taskChannelRewardAndUsersContainer">
                         <div className="taskChannelRewardContainer">
                             <img src={tonusdtIMG}/>
-                            <h2>{tonToUsdRate ? tonBalance.toFixed(2) : "…"} TON | {usdtBalance.toFixed(2)} USDT</h2> 
+                            <h2>{tonToUsdRate ? tonBalance : "…"} TON | {usdtBalance.toFixed(2)} USDT</h2> 
                         </div>
                         <div className="taskChannelUsersContainer">
                             <img src={usersIMG}/>
