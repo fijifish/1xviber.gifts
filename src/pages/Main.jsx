dc56b7818d1630c9211c970492ea0d248cd7dd19
НовыйИмя <Email>
2025-10-10 15:31:32 +0200

    ddd

--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -187,7 +187,7 @@ const OnexGifts = () => {
                         <img src={channelIMG}/>
                         <div class="textChannelNameContainer">
                             <div class="textChannelNameContainerPart1">
-                                AIMI Traffi
+                                AIMI Traffic
                             </div>
                             <div class="textChannelNameContainerPart2">
                                 Канал
