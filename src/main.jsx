b5f4837cea1b386856d238c55c366bfbe9e020a5
НовыйИмя <Email>
2025-10-10 13:07:17 +0200

    ddd

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
-import { TonConnectUIProvider } from "@tonconnect/ui-react";
 import App from "./App.jsx";
 import { UserProvider } from "./UserContext.jsx";
 
