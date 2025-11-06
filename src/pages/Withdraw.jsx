6c438eba357f7701ea5e0c36205e3bfcd7cb4f0e
НовыйИмя <Email>
2025-11-06 23:12:14 +0100

    dfvd

--- src/pages/Withdraw.jsx
+++ src/pages/Withdraw.jsx
@@ -306,7 +306,7 @@ export default function Withdraw() {
                             style={{
                             display: "flex",
                             alignItems: "center",
-                            justifyContent: "space-around",
+                            justifyContent: "space-between",
                             width: "100%",
                             gap: "1vh"
                             }}
